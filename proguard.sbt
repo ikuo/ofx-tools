1a078bd29de1528868e39777202b1e615e91c60c
Ikuo Matsumura <makiczar@gmail.com>
2016-06-04 00:43:23 +0900

    Skeleton of s3 client

--- proguard.sbt
+++ proguard.sbt
@@ -3,6 +3,17 @@ proguardSettings
 ProguardKeys.options in Proguard ++= Seq("-dontnote", "-dontwarn", "-ignorewarnings")
 
 ProguardKeys.options in Proguard += """
+-optimizations !code/simplification/arithmetic,!field/*,!class/merging/*,!code/allocation/variable
+-keep public class org.apache.commons.logging.impl.Jdk14Logger { *** <init>(...); }
 -keep public class net.shiroka.tools.ofx.aws.Lambda { *; }
 -keepnames public class com.amazonaws.services.lambda.runtime.Context
+-keep public class org.apache.commons.logging.impl.LogFactoryImpl
 """
+
+ProguardKeys.merge in Proguard := true
+
+ProguardKeys.mergeStrategies in Proguard += ProguardMerge.discard("META-INF/.*".r)
+
+ProguardKeys.mergeStrategies in Proguard += ProguardMerge.append("reference.conf")
+
+scalacOptions += "-target:jvm-1.7"
--- src/main/scala/net/shiroka/tools/ofx/aws/Lambda.scala
+++ src/main/scala/net/shiroka/tools/ofx/aws/Lambda.scala
@@ -1,11 +1,38 @@
 package net.shiroka.tools.ofx.aws
 
+import java.io._
+import com.amazonaws.regions.Regions
 import com.amazonaws.services.lambda.runtime.{ Context, LambdaLogger }
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+import com.netaporter.uri.Uri.parse
+import com.typesafe.config.ConfigFactory
+import net.shiroka.tools.ofx.ShinseiBankOfxGeneration
 
 class Lambda {
+  val awsConfig = ConfigFactory.load("net.shiroka.tools.ofx.aws")
+
   def handler(uri: String, context: Context): String = {
     val lambdaLogger: LambdaLogger = context.getLogger()
     lambdaLogger.log("uri = " + uri)
-    uri
+    handleUri(uri)
+  }
+
+  def handleUri(uriStr: String): String = {
+    val uri = parse(uriStr)
+    val generation =
+      uri.pathParts.takeRight(3).map(_.part).toList match {
+        case "shinsei-bank" :: accountNum :: fileName :: Nil =>
+          ShinseiBankOfxGeneration(accountNum.toLong)
+        case parts => sys.error(s"Unexpected path parts $parts")
+      }
+
+    val client = new AmazonS3Client(new DefaultAWSCredentialsProviderChain())
+      .withRegion(Regions.valueOf(awsConfig.getString("region")))
+
+    val src: InputStream = ???
+    val sink: PrintStream = ???
+    //generation(src, sink)
+    uriStr
   }
 }
