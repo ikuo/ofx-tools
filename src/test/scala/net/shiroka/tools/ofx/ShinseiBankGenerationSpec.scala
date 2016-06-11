0e47eed0e0397beb1a8a3514972adb4f95501390
Ikuo Matsumura <makiczar@gmail.com>
2016-06-11 21:15:22 +0900

    Fix spec

--- src/test/scala/net/shiroka/tools/ofx/ShinseiBankGenerationSpec.scala
+++ src/test/scala/net/shiroka/tools/ofx/ShinseiBankGenerationSpec.scala
@@ -1,6 +1,7 @@
 package net.shiroka.tools.ofx
 
 import java.io._
+import com.typesafe.config.ConfigFactory
 import org.specs2.mutable._
 import org.specs2.specification.Scope
 
@@ -8,10 +9,11 @@ class ShinseiBankConversionSpec extends SpecificationLike {
   "ShinseiBankConversion" >> {
     "#apply" >> {
       "it generates OFX statement" in {
-        val conversion = ShinseiBankConversion(6300215825L)
+        val config = ConfigFactory.load().getConfig("net.shiroka.tools.ofx.conversions.shinsei-bank")
+        val conversion = ShinseiBankConversion(config)
         val src = getClass.getResourceAsStream("/shinsei-bank.csv")
         val result = printToBaos(out => conversion(src, out)).toString
-        result must contain("<ACCTID>6300215825</ACCTID>")
+        result must contain("<ACCTID>1001000100</ACCTID>")
         result must contain("<STMTTRN>")
       }
     }
