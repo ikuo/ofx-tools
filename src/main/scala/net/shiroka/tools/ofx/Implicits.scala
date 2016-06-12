3f46ab0f1ea84874ade0468f87cb164e9fd6ef4c
Ikuo Matsumura <makiczar@gmail.com>
2016-06-12 16:46:25 +0900

    Remove unused reducePairs(..)

--- src/main/scala/net/shiroka/tools/ofx/Implicits.scala
+++ src/main/scala/net/shiroka/tools/ofx/Implicits.scala
@@ -1,13 +1,6 @@
 package net.shiroka.tools.ofx
 
 object Implicits {
-  implicit class ReducePairs[A](self: Iterable[(A, Iterator[Transaction])]) {
-    val empty = (Nil: List[A], Iterator.apply[Transaction]())
-
-    def reducePairs: (List[A], Iterator[Transaction]) =
-      self.foldLeft(empty) { case ((csvs, iter), (csv, txns)) => (csv :: csvs, iter ++ txns) }
-  }
-
   implicit class Tapper[T](self: T) {
     def tap[U](f: T => U): T = { f(self); self }
   }
